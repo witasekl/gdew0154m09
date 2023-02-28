ec53647972cc49e2a542495e5271465359542704
Libor Witasek <lwitasek@vendavo.com>
2023-02-28 08:16:53 +0100

    Fix incorrect variable name.

--- components/gdew0154m09/display.py
+++ components/gdew0154m09/display.py
@@ -43,8 +43,8 @@ async def to_code(config):
     dc = await cg.gpio_pin_expression(config[CONF_DC_PIN])
     cg.add(var.set_dc_pin(dc))
 
-    reset = await cg.gpio_pin_expression(config[CONF_BUSY_PIN])
-    cg.add(var.set_busy_pin(reset))
+    busy = await cg.gpio_pin_expression(config[CONF_BUSY_PIN])
+    cg.add(var.set_busy_pin(busy))
 
     reset = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
     cg.add(var.set_reset_pin(reset))
